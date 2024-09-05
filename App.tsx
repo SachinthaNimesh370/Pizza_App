cded9fb2f298143d430bfa2697ffdf11bcf1e035
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2024-09-05 09:07:48 +0530

    install react native navigator

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
+import AppNanigation from './src/navigations/AppNanigation';
 
 function App(): React.JSX.Element {
 
   return (
 
     <View style={sty.container}>
+      <AppNanigation/>
       {/* <LoginScreen/> */}
-      <SignUpScreen/>
+      {/* <SignUpScreen/> */}
     </View>
     
   );
---
+++ src/navigations/AppNanigation.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+const AppNanigation = () => {
+  return (
+    <NavigationContainer children={undefined}>
+        
+    </NavigationContainer>
+  )
+}
+
+export default AppNanigation
